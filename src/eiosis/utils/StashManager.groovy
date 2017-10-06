81376fa983a3a97c4816f45cd55511a09e7fd67e
John Smith <John.Smith@example.com>
2017-10-06 10:50:33 +0200

    SharedLibTest

---
+++ src/eiosis/utils/StashManager.groovy
@@ -0,0 +1,42 @@
+#!/usr/bin/groovy
+package eiosis.utils.StashManager
+
+class StashManager
+{
+    StashManager()
+    {
+        echo "StageManager constructor"
+    }
+
+// ----------------------------------------------------------------------------
+
+    def addArtifact(String inName, String inPath)
+    {
+        if (this.mArtifacts.contains(inName))
+        {
+            echo "Cannot duplicate stash entries"
+            return
+        }
+        echo "Stash mArtifacts:" + inPath + " as entry: " + inName
+        mArtifacts << inName
+        stash includes: inPath, name: inName
+    }
+
+    def dump()
+    {
+        echo mArtifacts
+    }
+
+    def unstash()
+    {
+        mArtifacts.each
+        { item ->
+            unstash name:item
+        }
+    }
+
+// ----------------------------------------------------------------------------
+
+    private Set<String> mArtifacts = []
+}
+
